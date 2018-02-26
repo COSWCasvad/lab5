dcc30e220fa32c5d43f2373562f143af489b6a20
Carlos Andres Sanchez Venegas <carlos.sanchez-v@mail.escuelaing.edu.co>
2018-02-26 17:36:44 -0500

    falta pruebas

--- src/test/java/edu/eci/cosw/samples/SpringDataRestApiApplicationTests.java
+++ src/test/java/edu/eci/cosw/samples/SpringDataRestApiApplicationTests.java
@@ -1,26 +1,68 @@
 package edu.eci.cosw.samples;
 
 
+import edu.eci.cosw.example.persistence.PatientsRepository;
+import edu.eci.cosw.jpa.sample.model.Paciente;
+import edu.eci.cosw.jpa.sample.model.PacienteId;
+import edu.eci.cosw.samples.services.PatientServices;
+import edu.eci.cosw.samples.services.ServicesException;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import junit.framework.Assert;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import static org.junit.Assert.fail;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
+@ActiveProfiles("test")
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SpringDataRestApiApplication.class)
 @WebAppConfiguration
 public class SpringDataRestApiApplicationTests {
-
+    @Autowired
+    private PatientServices ps;
+    
+    @Autowired
+    private PatientsRepository pr;
         
-	@Test
-	public void contextLoads() {
-	}
         
         @Test
-        public void patientLoadTest(){
-    
+        public void deberiaConsultarPacienteQueExiste(){
+            Paciente p = new Paciente(new PacienteId(1, "cc"), "carlossdad", new Date(5000));
+            pr.save(p);
+            System.out.println("------------------------ANTES--------------------------------");
+        try {
+            System.out.println(p.getNombre());
+            //System.out.println(ps.getPatient(1, "cc").getNombre());
+            System.out.println(ps.topPatients(0).get(0).getNombre());
+            
+        } catch (ServicesException ex) {
+            fail("Fail in test deberiaConsultarPacienteQueExiste");
         }
+        }
+        
+        @Test
+        public void noDeberiaConsultarPacienteQueExiste(){
+            
+        }
+        @Test
+        public void noDeberiaObtenerPacientesConMenosConsultas(){
+            
+        }
+        @Test
+        public void deberiaConsultarPacientesConNConsultas(){
+            
+        }
+
 
 }
